2ecdb28e6684c9d3dca122e20fd2da8da3349963
FlightBlaze <flightblaze@gmail.com>
2021-11-15 23:58:54 +0300

    Moving from Newtoo-GLFW. Initial commit

---
+++ include/blazevg.hh
@@ -0,0 +1,133 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <vector>
+#include <cmath>
+
+namespace bvg {
+
+enum class LineJoin {
+    Bevel,
+    Round,
+    Miter
+};
+
+enum class LineCap {
+    Butt,
+    Round,
+    Square
+};
+
+struct Color {
+    Color();
+    Color(float r, float g, float b, float a = 1.0f);
+    float r, g, b, a;
+};
+
+static const Color Black = Color(0.0f, 0.0f, 0.0f);
+static const Color White = Color(1.0f, 1.0f, 1.0f);
+
+struct Style {
+    enum class Type {
+        SolidColor,
+        LinearGradient
+    };
+    Type type;
+    Color color;
+    Color gradientStart;
+    Color gradientEnd;
+};
+
+Style SolidColor(Color color);
+Style LinearGradient(Color start, Color end);
+
+struct LineDash {
+    LineDash();
+    LineDash(float length, float gapLength, float offset = 0.0f);
+    float length, gapLength, offset;
+};
+
+namespace factory {
+
+struct TwoPolylines {
+    std::vector<glm::vec2> first, second;
+};
+
+struct TriangeIndices {
+    int a, b, c;
+};
+
+struct ShapeMesh {
+    std::vector<glm::vec2> vertices;
+    std::vector<TriangeIndices> indices;
+    
+    void add(ShapeMesh& b);
+};
+
+ShapeMesh strokePolyline(std::vector<glm::vec2>& points, const float diameter);
+ShapeMesh bevelJoin(std::vector<glm::vec2>& a, std::vector<glm::vec2>& b, const float diameter);
+ShapeMesh roundJoin(std::vector<glm::vec2>& a, std::vector<glm::vec2>& b, const float diameter);
+ShapeMesh miterJoin(std::vector<glm::vec2>& a, std::vector<glm::vec2>& b, const float diameter,
+                    float miterLimitAngle = M_PI_2 + M_PI_4);
+
+TwoPolylines dividePolyline(std::vector<glm::vec2>& points, float t);
+
+std::vector<std::vector<glm::vec2>> dashedPolyline(std::vector<glm::vec2>& points,
+                                                   float dashLength, float gapLength,
+                                                   float offset = 0.0f);
+
+ShapeMesh roundedCap(glm::vec2 position, glm::vec2 direction, const float diameter);
+ShapeMesh squareCap(glm::vec2 position, glm::vec2 direction, const float diameter);
+
+std::vector<TriangeIndices> createIndicesConvex(int numVertices);
+
+} // namespace factory
+
+class Context {
+public:
+    Context(float width, float height);
+    
+    glm::mat4 MVP = glm::mat4(1.0f);
+    
+    LineJoin lineJoin = LineJoin::Miter;
+    LineCap lineCap = LineCap::Butt;
+    LineDash lineDash = LineDash();
+    
+    Style fillStyle = SolidColor(Black);
+    Style strokeStyle = SolidColor(Black);
+    
+    int blurRadius = 0;
+    
+    void orthographic(float width, float height);
+    
+    void beginPath();
+    void closePath();
+    
+    virtual void convexFill();
+    virtual void fill();
+    virtual void stroke();
+    
+    void moveTo(float x, float y);
+    void lineTo(float x, float y);
+    void cubicTo(float cp1x, float cp1y, float cp2x, float cp2y, float x, float y);
+    void quadraticTo(float cpx, float cpy, float x, float y);
+    
+    void arc(float x, float y, float radius, float startAngle, float endAngle);
+    void rect(float x, float y, float width, float height, float radius);
+    void rect(float x, float y, float width, float height);
+    void rect(float x, float y, float width, float height,
+              float topLeftRadius, float topRightRadius,
+              float bottomRightRadius, float bottomLeftRadius);
+    
+protected:
+    std::vector<std::vector<glm::vec2>> mPolylines;
+    bool mIsPolylineClosed = false;
+    glm::vec2 mCurrentPos;
+    
+    std::vector<glm::vec2> toOnePolyline(std::vector<std::vector<glm::vec2>> polylines);
+    
+    factory::ShapeMesh internalFill();
+    factory::ShapeMesh internalConvexFill();
+    factory::ShapeMesh internalStroke();
+};
+
+} // namespace bvg
---
+++ source/blazevg.cc
@@ -0,0 +1,604 @@
+#include <blazevg.hh>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/vector_angle.hpp>
+
+namespace bvg {
+
+namespace factory {
+
+ShapeMesh strokePolyline(std::vector<glm::vec2>& points, const float diameter) {
+    float radius = diameter / 2.0f;
+    size_t numPoints = points.size();
+    ShapeMesh mesh;
+    
+    if(points.size() < 2)
+        return mesh;
+    
+    // Calculate arrays size
+    mesh.vertices.resize(numPoints * 2);
+    mesh.indices.resize(numPoints * 2 - 2);
+    
+    // For each point we add two points to the mesh and
+    // connect them with previous two points if any
+    for(size_t i = 0; i < numPoints; i++) {
+        // Check for tips
+        bool isStart = i == 0;
+        bool isEnd = i == numPoints - 1;
+
+        // Retrieve points
+        glm::vec2 currentPoint = points[i];
+        glm::vec2 backPoint = !isStart ? points[i - 1LL] : currentPoint;
+        glm::vec2 nextPoint = !isEnd   ? points[i + 1LL] : currentPoint;
+        
+        // Calculate three diections
+        glm::vec2 forwardDir  = glm::normalize(nextPoint - currentPoint);
+        glm::vec2 backwardDir = glm::normalize(backPoint - currentPoint) * -1.0f;
+        glm::vec2 meanDir;
+        
+        // Calculate mean of forward and backward directions
+        if (isStart) meanDir = forwardDir;
+        else if (isEnd) meanDir = backwardDir;
+        else meanDir = (forwardDir + backwardDir) / 2.0f;
+        
+        // Perpendicular to mean direction
+        glm::vec2 a = glm::vec2(meanDir.y, -meanDir.x) * radius;
+        glm::vec2 b = -a;
+        
+        // Put put vectors into the right place
+        a += currentPoint;
+        b += currentPoint;
+        
+        // Add vectors to mesh vertices
+        size_t currentVertexIndex = i * 2;
+        size_t previousVertexIndex = currentVertexIndex - 2;
+        mesh.vertices.at(currentVertexIndex) = a;
+        mesh.vertices.at(currentVertexIndex + 1LL) = b;
+        
+        // Connect vertices with bridge of two trianges
+        if(!isStart) {
+            int idxA = currentVertexIndex;
+            int idxB = currentVertexIndex + 1;
+            int idxC = previousVertexIndex;
+            int idxD = previousVertexIndex + 1;
+            TriangeIndices m { idxA, idxB, idxC };
+            TriangeIndices k { idxB, idxC, idxD };
+            size_t index = i * 2LL - 2;
+            mesh.indices.at(index) = m;
+            mesh.indices.at(index + 1LL) = k;
+        }
+    }
+    return mesh;
+}
+
+void ShapeMesh::add(ShapeMesh& b) {
+    int plusVertices = this->vertices.size();
+    int plusIndices = this->indices.size();
+    this->vertices.resize(this->vertices.size() + b.vertices.size());
+    this->indices.resize(this->indices.size() + b.indices.size());
+    for(int i = 0; i < b.vertices.size(); i++)
+        this->vertices[i + plusVertices] = b.vertices[i];
+    for(int i = 0; i < b.indices.size(); i++) {
+        TriangeIndices tri = b.indices[i];
+        tri.a += plusVertices;
+        tri.b += plusVertices;
+        tri.c += plusVertices;
+        this->indices[i + plusIndices] = tri;
+    }
+}
+
+bool isCurvesCorrectForJoining(std::vector<glm::vec2>& a, std::vector<glm::vec2>& b) {
+    if (a.size() < 2 || b.size() < 2)
+        return false;
+    return true;
+}
+
+ShapeMesh bevelJoin(std::vector<glm::vec2>& a, std::vector<glm::vec2>& b, const float diameter) {
+    float radius = diameter / 2.0f;
+    ShapeMesh mesh;
+    
+    if(!isCurvesCorrectForJoining(a, b))
+        return mesh;
+    
+    glm::vec2 center = b.at(0);
+    glm::vec2 dirA = glm::normalize(a.at(a.size() - 1) - a.at(a.size() - 2));
+    glm::vec2 dirB = glm::normalize(b.at(1) - b.at(0));
+    
+    // --A   C-__
+    //   |  /    --
+    // --B  D-__
+    //          --
+    
+    glm::vec2 Ad = glm::vec2(dirA.y, -dirA.x) * radius;
+    glm::vec2 Bd = -Ad;
+    glm::vec2 Cd = glm::vec2(dirB.y, -dirB.x) * radius;
+    glm::vec2 Dd = -Cd;
+    glm::vec2 A = Ad + center;
+    glm::vec2 B = Bd + center;
+    glm::vec2 C = Cd + center;
+    glm::vec2 D = Dd + center;
+    
+    float angle = glm::orientedAngle(dirA, dirB);
+    
+    mesh.vertices.resize(3);
+    mesh.vertices.at(0) = center;
+    if(angle > 0) {
+        mesh.vertices.at(1) = A;
+        mesh.vertices.at(2) = C;
+    }
+    else {
+        mesh.vertices.at(1) = B;
+        mesh.vertices.at(2) = D;
+    }
+    TriangeIndices tri { 0, 1, 2 };
+    mesh.indices.push_back(tri);
+    
+    return mesh;
+}
+
+std::vector<glm::vec2> quadraticBezier(glm::vec2 p0, glm::vec2 p1, glm::vec2 p2, int segments) {
+    auto points = std::vector<glm::vec2>(segments);
+    float step = 1.0f / (segments - 1);
+    float t = 0.0f;
+    for (int i = 0; i < segments; i++) {
+        glm::vec2 q0 = glm::mix(p0, p1, t);
+        glm::vec2 q1 = glm::mix(p1, p2, t);
+        glm::vec2 r = glm::mix(q0, q1, t);
+        points[i] = r;
+        t += step;
+    }
+    return points;
+}
+
+std::vector<glm::vec2> cubicBezier(glm::vec2 p0, glm::vec2 p1, glm::vec2 p2, glm::vec2 p3, int segments) {
+    auto points = std::vector<glm::vec2>(segments);
+    float step = 1.0f / (segments - 1);
+    float t = 0.0f;
+    for (int i = 0; i < segments; i++) {
+        glm::vec2 q0 = glm::mix(p0, p1, t);
+        glm::vec2 q1 = glm::mix(p1, p2, t);
+        glm::vec2 q2 = glm::mix(p2, p3, t);
+        glm::vec2 r0 = glm::mix(q0, q1, t);
+        glm::vec2 r1 = glm::mix(q1, q2, t);
+        glm::vec2 b = glm::mix(r0, r1, t);
+        points[i] = b;
+        t += step;
+    }
+    return points;
+}
+
+std::vector<TriangeIndices> createIndicesConvex(int numVertices) {
+    size_t amount = numVertices - 2;
+    auto indices = std::vector<TriangeIndices>(amount);
+    int k = 1;
+    for (size_t i = 0; i < amount; i++)
+    {
+        indices[i].a = 0;
+        
+        // Connect current edge with next
+        indices[i].b = k;
+        indices[i].c = k + 1;
+        k++;
+    }
+    return indices;
+}
+
+std::vector<glm::vec2> createArc(float startAngle, float endAngle, float radius, int segments, glm::vec2 offset) {
+    auto arcVerts = std::vector<glm::vec2>(segments);
+    float angle = startAngle;
+    float arcLength = endAngle - startAngle;
+    for (int i = 0; i <= segments - 1; i++) {
+        float x = sin(angle) * radius;
+        float y = cos(angle) * radius;
+
+        arcVerts[i] = glm::vec2(x, y) + offset;
+        angle += (arcLength / segments);
+    }
+    return arcVerts;
+}
+
+float positiveAngle(float angle) {
+    if(angle < 0) {
+        return M_PI * 2 + angle;
+    }
+    return angle;
+}
+
+ShapeMesh roundJoin(std::vector<glm::vec2>& a, std::vector<glm::vec2>& b, const float diameter) {
+    float radius = diameter / 2.0f;
+    ShapeMesh mesh;
+    
+    if(!isCurvesCorrectForJoining(a, b))
+        return mesh;
+    
+    glm::vec2 center = b.at(0);
+    glm::vec2 dirA = glm::normalize(a.at(a.size() - 1) - a.at(a.size() - 2));
+    glm::vec2 dirB = glm::normalize(b.at(1) - b.at(0));
+    
+    glm::vec2 Ad = glm::vec2(dirA.y, -dirA.x) * radius;
+    glm::vec2 Bd = -Ad;
+    glm::vec2 Cd = glm::vec2(dirB.y, -dirB.x) * radius;
+    glm::vec2 Dd = -Cd;
+    glm::vec2 A = Ad + center;
+    glm::vec2 B = Bd + center;
+    glm::vec2 C = Cd + center;
+    glm::vec2 D = Dd + center;
+    
+    float angle = glm::orientedAngle(dirA, dirB);
+    
+    static const int numCurveSegments = 32;
+    mesh.vertices.resize(1);
+    mesh.vertices.at(0) = center;
+    std::vector<glm::vec2> curve;
+    float start = 0;
+    float end = 0;
+    
+    glm::vec2 up;
+    
+    if(angle > 0) {
+        start = atan2(Ad.x, Ad.y);
+        end = atan2(Cd.x, Cd.y) - 0.1f;
+        up = Ad;
+    }
+    else {
+        start = atan2f(Bd.x, Bd.y);
+        end = atan2f(Dd.x, Dd.y) + 0.1f;
+        up = Bd;
+    }
+    curve = createArc(start, end, radius, 32, center);
+    
+    // if arc is inverted
+    if(glm::dot(glm::normalize(curve.at(15) - center), up) < 0) {
+        start = positiveAngle(start);
+        end = positiveAngle(end);
+        curve = createArc(start, end, radius, 32, center);
+    }
+    mesh.vertices.insert(mesh.vertices.end(), curve.begin(), curve.end());
+    std::vector<TriangeIndices> tris = createIndicesConvex(mesh.vertices.size());
+    mesh.indices.insert(mesh.indices.end(), tris.begin(), tris.end());
+    
+    return mesh;
+}
+
+glm::vec2 EliminationLineLineIntersection(glm::vec2 v1, glm::vec2 v2, glm::vec2 v3, glm::vec2 v4)
+{
+    float x12 = v1.x - v2.x;
+    float x34 = v3.x - v4.x;
+    float y12 = v1.y - v2.y;
+    float y34 = v3.y - v4.y;
+    float c = x12 * y34 - y12 * x34;
+    float a = v1.x * v2.y - v1.y * v2.x;
+    float b = v3.x * v4.y - v3.y * v4.x;
+    if(c != 0) {
+        float x = (a * x34 - b * x12) / c;
+        float y = (a * y34 - b * y12) / c;
+        return glm::vec2(x, y);
+    }
+    else {
+        // Lines are parallel
+        return glm::vec2(0.0f);
+    }
+}
+
+glm::vec2 LineLineIntersection(glm::vec2 v1, glm::vec2 v2, glm::vec2 v3, glm::vec2 v4) {
+    return EliminationLineLineIntersection(v1, v2, v3, v4);
+}
+
+ShapeMesh miterJoin(std::vector<glm::vec2>& a, std::vector<glm::vec2>& b, const float diameter,
+                    float miterLimitAngle) {
+    float radius = diameter / 2.0f;
+    ShapeMesh mesh;
+    
+    if(!isCurvesCorrectForJoining(a, b))
+        return mesh;
+    
+    glm::vec2 center = b.at(0);
+    glm::vec2 dirA = glm::normalize(a.at(a.size() - 1) - a.at(a.size() - 2));
+    glm::vec2 dirB = glm::normalize(b.at(1) - b.at(0));
+    
+    float angle = glm::orientedAngle(dirA, dirB);
+    
+    if (fabsf(angle) > miterLimitAngle)
+        return bevelJoin(a, b, diameter);
+    
+    glm::vec2 Ad = glm::vec2(dirA.y, -dirA.x) * radius;
+    glm::vec2 Bd = -Ad;
+    glm::vec2 Cd = glm::vec2(dirB.y, -dirB.x) * radius;
+    glm::vec2 Dd = -Cd;
+    glm::vec2 A = Ad + center;
+    glm::vec2 B = Bd + center;
+    glm::vec2 C = Cd + center;
+    glm::vec2 D = Dd + center;
+    
+    mesh.vertices.resize(4);
+    mesh.vertices.at(0) = center;
+    if(angle > 0) {
+        glm::vec2 I = LineLineIntersection(A, A + dirA, C, C + dirB);
+        mesh.vertices.at(1) = A;
+        mesh.vertices.at(2) = C;
+        mesh.vertices.at(3) = I;
+    }
+    else {
+        glm::vec2 I = LineLineIntersection(B, B + dirA, D, D + dirB);
+        mesh.vertices.at(1) = B;
+        mesh.vertices.at(2) = D;
+        mesh.vertices.at(3) = I;
+    }
+    //   0
+    //  / \
+    // 1---2
+    //  \ /
+    //   3
+    std::vector<TriangeIndices> tris = {
+        { 0, 1, 2 },
+        { 1, 2, 3 }
+    };
+    mesh.indices.insert(mesh.indices.end(), tris.begin(), tris.end());
+    
+    return mesh;
+}
+
+TwoPolylines dividePolyline(std::vector<glm::vec2>& points, float t) {
+    TwoPolylines twoLines;
+    if (t <= 0) {
+        twoLines.second = points;
+        return twoLines;
+    }
+    if (t >= 1) {
+        twoLines.first = points;
+        return twoLines;
+    }
+    float remapedT = t * (float)(points.size() - 1);
+    int segmentIdx = (int)floorf(remapedT);
+    float segmentT = remapedT - (float)segmentIdx;
+    
+    twoLines.first.resize(segmentIdx + 2);
+    for (int i = 0; i < segmentIdx + 1; i++) {
+        twoLines.first.at(i) = points.at(i);
+    }
+    twoLines.first.at(segmentIdx + 1) = glm::mix(points.at(segmentIdx), points.at(segmentIdx + 1), segmentT);
+    
+    twoLines.second.resize(points.size() - segmentIdx);
+    for (int i = 1; i < twoLines.second.size(); i++){
+        twoLines.second.at(i) = points.at(segmentIdx + i);
+    }
+    twoLines.second.at(0) = glm::mix(points.at(segmentIdx), points.at(segmentIdx + 1), segmentT);
+    
+    return twoLines;
+}
+
+float lengthOfPolyline(std::vector<glm::vec2>& points) {
+    if (points.size() < 2)
+        return 0.0f;
+
+    glm::vec2 previousPoint = points.at(0);
+    float length = 0.0f;
+    for (size_t i = 1; i < points.size(); i++) {
+        length += glm::distance(points.at(i), previousPoint);
+        previousPoint = points.at(i);
+    }
+    return length;
+}
+
+std::vector<float> measurePolyline(std::vector<glm::vec2>& points) {
+    std::vector<float> lengths;
+    lengths.resize(points.size() - 1); // number of segments
+    for (size_t i = 0; i < lengths.size(); i++) {
+        lengths.at(i) = glm::distance(points.at(i), points.at(i + 1LL));
+    }
+    return lengths;
+}
+
+float tAtLength(float length, std::vector<float>& lengths) {
+    size_t pointBeforeLength = 0;
+    float previousLength = 0.0f;
+    float currentLength = 0.0f;
+    float localT = 0.0f;
+    if(lengths.size() > 0) {
+        localT = fminf(length, lengths.at(0));
+    }
+    for (size_t i = 0; i < lengths.size(); i++) {
+        previousLength = currentLength;
+        currentLength += lengths.at(i);
+        if (length < currentLength) {
+            pointBeforeLength = i;
+            localT = (length - previousLength) / lengths.at(i);
+            break;
+        }
+    }
+    float t = ((float)pointBeforeLength + localT) / (float)lengths.size();
+    return t;
+}
+
+std::vector<std::vector<glm::vec2>> dashedPolyline(std::vector<glm::vec2>& points,
+                                                   float dashLength, float gapLength,
+                                                   float offset) {
+    std::vector<std::vector<glm::vec2>> lines;
+    std::vector<glm::vec2> currentPath = points;
+    
+    float dashGapLength = dashLength + gapLength;
+    float offsetTimes = floorf(fabsf(offset) / dashGapLength);
+    float localOffset = fabsf(offset) - offsetTimes * dashGapLength;
+    if(offset > 0) {
+        std::vector<float> lengths = measurePolyline(currentPath);
+        if(localOffset > gapLength) {
+            float startDashLength = localOffset - gapLength;
+            lines.push_back(dividePolyline(currentPath, tAtLength(startDashLength, lengths)).first);
+        }
+        currentPath = dividePolyline(currentPath, tAtLength(localOffset, lengths)).second;
+    }
+    else if(offset < 0) {
+        std::vector<float> lengths = measurePolyline(currentPath);
+        if(localOffset < dashLength) {
+            float startDashLength = dashLength - localOffset;
+            lines.push_back(dividePolyline(currentPath, tAtLength(startDashLength, lengths)).first);
+        }
+        currentPath = dividePolyline(currentPath, tAtLength(dashGapLength - localOffset,
+                                                            lengths)).second;
+    }
+    
+    static const int maxDashes = 999;
+    for(int i = 0; i < maxDashes; i++) {
+        std::vector<float> lengths = measurePolyline(currentPath);
+        TwoPolylines twoPathes = dividePolyline(currentPath, tAtLength(dashLength, lengths));
+        if(twoPathes.first.size() < 2)
+            break;
+        lines.push_back(twoPathes.first);
+        if(twoPathes.second.size() < 2)
+            break;
+        std::vector<float> secondLengths = measurePolyline(twoPathes.second);
+        currentPath = dividePolyline(twoPathes.second, tAtLength(gapLength, secondLengths)).second;
+        if(currentPath.size() < 2)
+            break;
+    }
+    return lines;
+}
+
+ShapeMesh roundedCap(glm::vec2 position, glm::vec2 direction, const float diameter) {
+    float radius = diameter / 2.0f;
+    ShapeMesh mesh;
+    
+    glm::vec2 Ad = glm::vec2(direction.y, -direction.x) * radius;
+    glm::vec2 Bd = -Ad;
+    
+    static const int numCurveSegments = 32;
+    mesh.vertices.resize(1);
+    mesh.vertices.at(0) = position;
+    
+    float dirAngle = atan2f(direction.x, direction.y);
+    float start = glm::orientedAngle(direction, Ad) + dirAngle;
+    float end = glm::orientedAngle(direction, Bd) + dirAngle + 0.1f;
+    
+    std::vector<glm::vec2> curve = createArc(start, end, radius, 32, position);
+    mesh.vertices.insert(mesh.vertices.end(), curve.begin(), curve.end());
+    
+    std::vector<TriangeIndices> tris = createIndicesConvex(mesh.vertices.size());
+    mesh.indices.insert(mesh.indices.end(), tris.begin(), tris.end());
+    
+    return mesh;
+}
+
+ShapeMesh squareCap(glm::vec2 position, glm::vec2 direction, const float diameter) {
+    std::vector<glm::vec2> points {
+        position,
+        position + glm::normalize(direction) * diameter
+    };
+    return strokePolyline(points, diameter);
+}
+
+} // namespace factory
+
+Color::Color():
+    r(0.0f), g(0.0f), b(0.0f), a(1.0f)
+{
+}
+
+Color::Color(float r, float g, float b, float a):
+    r(r), g(g), b(b), a(a)
+{
+}
+
+LineDash::LineDash()
+    : length(10.0f), gapLength(0.0f), offset(0.0f)
+{
+}
+LineDash::LineDash(float length, float gapLength, float offset)
+: length(length), gapLength(gapLength), offset(offset)
+{
+}
+
+Style SolidColor(Color color) {
+    Style style;
+    style.type = Style::Type::SolidColor;
+    style.color = color;
+    return style;
+}
+
+Style LinearGradient(Color start, Color end) {
+    Style style;
+    style.type = Style::Type::LinearGradient;
+    style.gradientStart = start;
+    style.gradientEnd = end;
+    return style;
+}
+
+Context::Context(float width, float height) {
+    this->orthographic(width, height);
+}
+
+void Context::orthographic(float width, float height) {
+    this->MVP = glm::ortho(0.0f, width, height, 0.0f, -1000.0f, 1000.0f);
+}
+
+void Context::beginPath() {
+    this->mPolylines.clear();
+    this->mIsPolylineClosed = false;
+    this->mCurrentPos = glm::vec2(0.0f);
+}
+
+void Context::closePath() {
+    if(this->mIsPolylineClosed || this->mPolylines.size() < 2)
+        return;
+    
+    this->mIsPolylineClosed = true;
+    
+    std::vector<glm::vec2> line {
+        this->mPolylines.back().back(),
+        this->mPolylines.front().front()
+    };
+    
+    this->mPolylines.push_back(line);
+}
+
+void Context::moveTo(float x, float y) {
+    mCurrentPos = glm::vec2(x, y);
+}
+
+void Context::lineTo(float x, float y) {
+    std::vector<glm::vec2> line {
+        this->mPolylines.back().back(),
+        glm::vec2(x, y)
+    };
+    mPolylines.push_back(line);
+}
+
+void Context::cubicTo(float cp1x, float cp1y, float cp2x, float cp2y, float x, float y) {
+    std::vector<glm::vec2> curve = factory::cubicBezier(mCurrentPos,
+                                                        glm::vec2(cp1x, cp1y),
+                                                        glm::vec2(cp2x, cp2y),
+                                                        glm::vec2(x, y),
+                                                        32);
+    mPolylines.push_back(curve);
+}
+
+void Context::quadraticTo(float cpx, float cpy, float x, float y) {
+    std::vector<glm::vec2> curve = factory::quadraticBezier(mCurrentPos,
+                                                        glm::vec2(cpx, cpy),
+                                                        glm::vec2(x, y),
+                                                        32);
+    mPolylines.push_back(curve);
+}
+
+std::vector<glm::vec2> Context::toOnePolyline(std::vector<std::vector<glm::vec2>> polylines) {
+    std::vector<glm::vec2> onePolyline;
+    if(polylines.size() == 0)
+        return onePolyline;
+    onePolyline = polylines.front();
+    for(int i = 1; i < polylines.size(); i++) {
+        std::vector<glm::vec2> ongoing = polylines.at(i);
+        
+        // Remove previous point (duplicate)
+        ongoing.pop_back();
+        
+        onePolyline.insert(onePolyline.begin(), ongoing.begin(), ongoing.end());
+    }
+    return onePolyline;
+}
+
+factory::ShapeMesh Context::internalConvexFill() {
+    factory::ShapeMesh mesh;
+    mesh.vertices = this->toOnePolyline(mPolylines);
+    mesh.indices = factory::createIndicesConvex(mesh.vertices.size());
+    return mesh;
+}
+
+} // namespace bvg
